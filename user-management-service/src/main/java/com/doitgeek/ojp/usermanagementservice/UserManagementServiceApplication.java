93fb5b1090bf27079cd42cc6a4a2f5eee6d21b68
Sainath Parkar <saiparkar4@gmail.com>
2019-08-18 23:08:17 +0530

    Added user management microservice.

---
+++ user-management-service/src/main/java/com/doitgeek/ojp/usermanagementservice/UserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.doitgeek.ojp.usermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserManagementServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementServiceApplication.class, args);
+    }
+}
