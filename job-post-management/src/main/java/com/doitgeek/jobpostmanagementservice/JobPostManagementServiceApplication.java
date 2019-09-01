a3f8e152c07f6d2bd7a49bfcf1cc77e74443906f
Sainath Parkar <saiparkar4@gmail.com>
2019-09-01 20:00:16 +0530

    Added new microservice for job post management. Closes #5

---
+++ job-post-management/src/main/java/com/doitgeek/jobpostmanagementservice/JobPostManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.doitgeek.jobpostmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class JobPostManagementServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobPostManagementServiceApplication.class, args);
+    }
+}
